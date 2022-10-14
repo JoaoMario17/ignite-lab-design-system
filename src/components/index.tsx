418c45725cb82a5a2562689a14025904994a68c3
Joao Mario <joao.martins@soluti.com.br>
2022-10-14 18:30:12 -0300

    feat: configuring tests and mock

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+import { Button } from './Button';
+import { Checkbox } from './Checkbox';
+import { Heading } from './Heading'
+import { Text } from './Text'
+import { TextInput } from './TextInput'
+
+export { Button, Heading, Checkbox, Text, TextInput}
